b7b2a7407cd27e8139f4ca36d734c53b213b8977
behzadiziy <behzadiziy@gmail.com>
2025-06-23 14:59:40 +0330

    seed an admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
         ]);
     }
 }
