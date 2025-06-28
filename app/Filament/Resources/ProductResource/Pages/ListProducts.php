c66872b2365c2e60a8883d962f54555db2887b26
behzadiziy <behzadiziy@gmail.com>
2025-06-28 13:50:37 +0330

    add table tab to product table

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\ProductResource\Pages;
 
-use App\Filament\Resources\ProductResource;
+use App\Enums\ProductStatus;
 use Filament\Actions;
+use App\Models\Product;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProductResource;
 
 class ListProducts extends ListRecords
 {
@@ -14,6 +18,18 @@ class ListProducts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All')
+                ->badge(Product::count()),
+
+            'unsynced' => Tab::make('Not Synchronized')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', "!=", ProductStatus::Synced->value))
+                ->badge(Product::where('status', '!=', ProductStatus::Synced->value)->count()),
         ];
     }
 }
