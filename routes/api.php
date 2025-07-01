1797f3a8f687212cb8374f2504752a8accec2178
behzadiziy <behzadiziy@gmail.com>
2025-07-01 14:53:57 +0330

    add tenants routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 // routes/api.php
 
-use App\Http\Controllers\Api\V1\MagentoCustomerController;
-use App\Http\Controllers\Api\V1\StoresController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\V1\StoresController;
 use App\Http\Controllers\Api\V1\MagentoSyncController;
 use App\Http\Controllers\Api\V1\AdminProductController;
+use App\Http\Controllers\Api\V1\Tenant\OrderController;
+use App\Http\Controllers\Api\V1\Tenant\ProductController;
+use App\Http\Controllers\Api\V1\MagentoCustomerController;
 use App\Http\Controllers\Api\V1\ProductIngestionController;
 
 /*
@@ -26,8 +28,9 @@ Route::middleware('auth:sanctum')->prefix('v1/admin')->group(function () {
     Route::post('products/{product}/sync', MagentoSyncController::class);
 
     Route::post('stores', [StoresController::class, 'create']);
- //   Route::post('stores/sync', [MagentoSyncController::class, 'sync']);
-    Route::get('/magento/customers', [MagentoCustomerController::class, 'getCustomers']);
+    //Route::post('stores/sync', [MagentoSyncController::class, 'sync']);
+    //Route::get('/magento/customers', [MagentoCustomerController::class, 'getCustomers']);
 
-
+    Route::apiResource('tenant/products', ProductController::class);
+    Route::apiResource('tenant/orders', OrderController::class);
 });
