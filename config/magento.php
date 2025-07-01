0761a95ea9a483243dee782743b03969b4c74335
behzadiziy <behzadiziy@gmail.com>
2025-07-01 14:53:25 +0330

    add new config to config/magento

--- config/magento.php
+++ config/magento.php
@@ -5,4 +5,10 @@ return [
         'url' => env('MAGENTO_URL'),
         'token' => env('MAGENTO_API_TOKEN'),
     ],
+
+    'api_url' => env('MAGENTO_API_URL'),
+    'username' => env('MAGENTO_API_USERNAME'),
+    'password' => env('MAGENTO_API_PASSWORD'),
+    'token' => env('MAGENTO_API_TOKEN'),
+
 ];
